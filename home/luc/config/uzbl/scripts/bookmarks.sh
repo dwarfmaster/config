24249e7b8a450cd4939e40a4f08a46fef52280e9
lucas8 <luc.linux@mailoo.org>
2014-07-28 09:52:36 +0200

    Removed debug output in uzbl script bookmarks.sh

--- home/luc/config/uzbl/scripts/bookmarks.sh
+++ home/luc/config/uzbl/scripts/bookmarks.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 cmd=$(echo $1 | perl -e 'my $in = <>; if($in =~ m/^(.*) "(.*)"/) { print "$1\t$2"; } else { print ""; }')
-echo "Cmd : \"$cmd\" ..."
 if [ -z "$cmd" ]
 then
-    echo "... is empty"
     name=$1
     tags=""
 else
     name=$(echo "$cmd" | cut -f1)
     tags=$(echo "$cmd" | cut -f2)
-    echo "... includes \"$name\" and \"$tags\""
 fi
-echo "Bookmarking $UZBL_URI with \"$name\" [$tags] from \"$1\""
 jot bookmark --skip-editor --notes "$name" $UZBL_URI $tags
 
