dad88a14c9484d5a515c78dab64e2824f0beaa8d
lucas8 <luc.linux@mailoo.org>
2014-07-31 23:14:41 +0200

    newsbeuter-listener now listen through localhost:7777.

--- home/luc/Prog/newsbeuter-listener.pl
+++ home/luc/Prog/newsbeuter-listener.pl
@@ -2,27 +2,36 @@
 
 use strict;
 use warnings;
+use IO::Socket;
 
-my $path = "/tmp/newsbeuter.fifo";
+my $port = '7777';
 my $browser = "uzbl-browser";
 
 exit if fork() != 0;
 
-system("mkfifo", $path);
-my $fifo;
-open($fifo, "+< $path") or die "Couldn't create and open $path name pipe.";
+my $sock = new IO::Socket::INET(
+    LocalHost => 'localhost',
+    LocalPort => $port,
+    Proto     => 'tcp',
+    Listen    => 1,
+    Reuse     => 1
+);
+die "Could not create socket : $!\n" unless $sock;
 
-while(my $in = <$fifo>)
-{
-    if($in =~ m/^open (.*)$/) {
-        if(fork() == 0) {
-            system($browser, $1);
+while(1) {
+    my $nsock = $sock->accept();
+    while(my $in = <$nsock>) {
+        if($in =~ m/^open (.*)$/) {
+            if(fork() == 0) {
+                system($browser, $1);
+            }
+        } else {
+            print $in;
         }
-    } else {
-        print STDERR $in;
     }
+    close $nsock;
 }
 
-close $fifo;
+close $sock;
 exit(0);
 
