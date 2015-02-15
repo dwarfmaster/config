f5abc9bc5e80da8b0da54765840c60671c1c9f18
lucas8 <luc.linux@mailoo.org>
2015-02-15 09:35:25 +0100

    Improved history opening in uzbl.

--- home/luc/config/uzbl/scripts/open_history.sh
+++ home/luc/config/uzbl/scripts/open_history.sh
@@ -2,6 +2,7 @@
 
 DMENU_SCHEME="history"
 DMENU_OPTIONS="xmms vertical resize"
+HISTORY_SIZE=100
 
 . "$UZBL_UTIL_DIR/dmenu.sh"
 . "$UZBL_UTIL_DIR/uzbl-dir.sh"
@@ -11,11 +12,11 @@ DMENU_OPTIONS="xmms vertical resize"
 # choose from all entries, sorted and uniqued
 if [ -z "$DMENU_HAS_VERTICAL" ]; then
     current="$( tail -n 1 "$UZBL_HISTORY_FILE" | cut -d ' ' -f 3 )"
-    goto="$( ( echo "$current"; awk '{ print $3 }' "$UZBL_HISTORY_FILE" | grep -v "^$current\$" | sort -u ) | $DMENU )"
+    goto="$( ( tail -n $HISTORY_SIZE "$UZBL_HISTORY_FILE" | awk '{ print $3 }' | grep -v "^$current\$" | sort -u ) | $DMENU )"
 else
     # choose an item in reverse order, showing also the date and page titles
     # pick the last field from the first 3 fields. this way you can pick a url (prefixed with date & time) or type just a new url.
-    goto="$( tac "$UZBL_HISTORY_FILE" | $DMENU | cut -d ' ' -f -3  | awk '{ print $NF }' )"
+    goto="$( tail -n $HISTORY_SIZE "$UZBL_HISTORY_FILE" | tac | $DMENU | cut -d ' ' -f -3  | awk '{ print $NF }' )"
 fi
 
 [ -n "$goto" ] && /home/LOG1/.config/uzbl/scripts/open.pl "$goto"
